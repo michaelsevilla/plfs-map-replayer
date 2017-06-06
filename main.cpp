b4e7f7a511915d068c1bcc1a1dfe7727a43862cd
Michael Sevilla <mikesevilla3@gmail.com>
2017-06-05 21:38:41 -0700

    verbose output
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- main.cpp
+++ main.cpp
@@ -67,7 +67,11 @@ int main(int argc, char ** argv)
             cout << "To Read File" << endl;
         }  
     }
-    printf( "Hello from process %d of %d\n", myrank, size );
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+    printf( "Hello from processor_name=%s, rank=%d of size=%d\n", 
+            processor_name, myrank, size );
 
     //all ranks open a file for writing together
     start = MPI_Wtime();
